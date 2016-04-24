39201856e4fc6b7a51fcd3b17b8c1228d5237213
Diogenes Oliveira <diogenes1oliveira+github@gmail.com>
2016-04-24 17:05:52 -0300

    Initial scheleton for windowinspector classes

---
+++ coruja/windowinspector.py
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+class Window:
+    def __init__(self, wid, title, pid):
+        pass
+
+    @property
+    def process_name(self):
+        pass
+
+
+class WindowInspector:
+    def __init__(self, on_activate):
+        pass
+
+    def get_active_windows(self):
+        pass
+
+    def get_active_window(self):
+        pass
